8f841f221e38564a80a1affb1d875a7a3b46e6bf
morganjander <19morgan89@gmail.com>
2019-05-31 16:12:26 +0200

    first 3 functions and header file

---
+++ libft/ft_memalloc.c
@@ -0,0 +1,11 @@
+#include "libft.h"
+
+void	*ft_memalloc(size_t size)
+{
+	void *mem;
+
+	mem = malloc(size);
+	if (!mem)
+		return (NULL);
+	ft_bzero(mem, size);
+}
---
+++ libft/ft_memset.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	char *ptr;
+	size_t i;
+
+	ptr = b;
+	i = 0;
+	while (i < len)
+		*(ptr + i++) = c;
+}
