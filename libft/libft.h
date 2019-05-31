5ad9f10cff7899f9c5b00345c33f085df376f0ea
morganjander <19morgan89@gmail.com>
2019-05-31 17:20:24 +0200

    one more function

---
+++ libft/libft.h
@@ -0,0 +1,9 @@
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+
+void	ft_memset(void *, int, size_t);
+void	ft_bzero(void *, size_t);
+void	ft_memalloc(size_t);
+void	ft_putchar_fd(char, int);
