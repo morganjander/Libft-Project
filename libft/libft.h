e0d34e3a6000fe401a3f1595ad949c6d3e484970
morganjander <19morgan89@gmail.com>
2019-06-13 12:19:44 +0200

    fixed libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -53,7 +53,7 @@ int                 ft_isascii(int c);
 int                 ft_isprint(int c);
 int                 ft_toupper(int c);
 int                 ft_tolower(int c);
-
+//Part2
 void                *ft_memalloc(size_t size);
 void                ft_memdel(void **ap);
 char				*ft_strnew(size_t size);
